4466a9e8bf0f9cf8d39256d798f2e2648a2505f3
Asiberus <raphael.beekmann@gmail.com>
2018-03-06 18:06:04 +0100

    GildedRose.java comment

--- src/main/java/fr/esiea/GildedRose.java
+++ src/main/java/fr/esiea/GildedRose.java
@@ -3,28 +3,36 @@ package fr.esiea;
 class GildedRose {
     Item[] items;
 
+    //Constructeur
     public GildedRose(Item[] items) {
         this.items = items;
     }
-  
+
+    //Fonction met a jour la qualité d'un item en fonction de son type.
     public void updateQuality(){
-        for (int i = 0; i < items.length; ++i) {
-            if(items[i].name.equals("Sulfuras, Hand of Ragnaros")) {items[i].quality = 80;}
-            else {items[i].sellIn -= 1;
-                if(items[i].name.equals("Aged Brie")) {updateAged(items[i]);}
-                else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {updateBackstage(items[i]);}
-                else {updateQuality(items[i]);}
-                if (items[i].quality > 50) {items[i].quality = 50;}
-                if (items[i].quality < 0) {items[i].quality = 0;}
+        for (Item item : items) {
+            if(item.name.equals("Sulfuras, Hand of Ragnaros")) {item.quality = 80;}
+            else {item.sellIn -= 1;
+                if(item.name.equals("Aged Brie")) {updateAgedBrie(item);}
+                else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {updateBackstage(item);}
+                else {updateQuality(item);}
+                if (item.quality > 50) {item.quality = 50;}
+                if (item.quality < 0) {item.quality = 0;}
             }
         }
     }
 
-    private void updateAged(Item item){
+    //Fonction augmentant la qualité d'un objet de type Aged Brie.
+    //Si la date de vente de l'objet est inférieur à 0, la qualité augmente de 2
+    private void updateAgedBrie(Item item){
         item.quality += 1;
         if (item.sellIn < 0) {item.quality += 1;}
     }
 
+    //Fonction personnalisé pour l'item de type Backstage passe concert.
+    //Si la date de vente est inférieur ou égale à 10, la qualité augmente de 2.
+    //Si la date de vente est inférieur ou égale à 5, la qualité augmente de 3.
+    //Si la date de vente est inférieur ou égale à 0, la qualité passe à 0.
     private void updateBackstage(Item item){
         item.quality += 1;
         if (item.sellIn <= 10) {item.quality += 1;}
@@ -32,6 +40,10 @@ class GildedRose {
         if (item.sellIn <= 0) {item.quality = 0;}
     }
 
+    //Fonction servant à actualiser la qualité d'un objet consomable.
+    //Par default La qualité baisse de 1  chaque jour.
+    //Si l'objet est conjured il baisse de 2 de qualité chaque jour.
+    //Si l'objet et conjured et la date de vente est dépassé, l'objet perd de 3 de qualité chaque jour.
     private void updateQuality(Item item){
         item.quality -= 1;
         if (item.isConjured) {item.quality -= 1;}
